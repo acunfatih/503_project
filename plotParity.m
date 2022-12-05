78983ea2987876a353c0afbfd1cfff34cdf54a03
KelseyEng <74799106+KelseyEng@users.noreply.github.com>
2022-12-05 14:44:25 -0500

    Improvements to appearance of parity plot

--- plotParity.m
+++ plotParity.m
@@ -3,7 +3,20 @@
 function plotParity(YTrain,YPred)
 
     figure
-    scatter(YTrain,YPred)
+    scatter(YTrain,YPred,'filled','MarkerFaceAlpha',.3,'MarkerEdgeAlpha',.3)
+    hold on
+    rangeVal = range([YTrain,YPred]);
+    minVal = min([YTrain,YPred]);
+    minVal = minVal - rangeVal*.1;
+    maxVal = max([YTrain,YPred]);
+    maxVal = maxVal + rangeVal*.1;
+    ylim([minVal,maxVal])
+    xlim([minVal,maxVal])
+    plot([minVal,maxVal],[minVal,maxVal],'r--')
+    set(gcf,'color','white')
+    legend('Data Points','y = x Reference Line','Location','NorthWest')
+    xlabel('Actual')
+    ylabel('Predicted')
     
 
 end
