96b4722e3f6d8170715d80e0e4f8a72ebc6df346
beibitlife <zharylkassynorfo@gmail.com>
2022-12-06 17:18:15 -0500

    SERA
    implemented using Gaussian pdf

--- seraCost.m
+++ seraCost.m
@@ -1,6 +1,11 @@
 function cost = seraCost(YPred, YTrain)
 
-phi_val = fitProbabilisticLoss(YTrain, 0); %relevance values
+a = (mean(YTrain)/std(YTrain))^2;
+b = (std(YTrain))^2/(mean(YTrain));
+phi_val = gampdf(YTrain, a); %relevance values
+phi_val = (phi_val - min(phi_val))/range(phi_val);
+phi_val = 1-phi_val;
+
 
 T = 1000;
 %exclude the first and the last phi terms
@@ -19,4 +24,11 @@ term2 = 0.5*mean((YPred(indx2) - YTrain(indx2)).^2);
 
 %summing 1001 terms
 cost = (term1 + term2 + inter_terms)/T;
+
+function [norm_data,minData,rangeData] = norm_zero2one(data)
+    minData = min(data);
+    rangeData = range(data);
+    norm_data  = (data - minData)./rangeData;
+end
+
 end
