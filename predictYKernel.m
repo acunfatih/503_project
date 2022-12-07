cb539cd13ef815d0ee21fb905b773c2f0873f5a7
Fatih Acun <fatih.acunn@gmail.com>
2022-12-07 16:57:24 -0500

    prediction implemented for kernel

--- predictYKernel.m
+++ predictYKernel.m
@@ -1,13 +1,23 @@
 % Predicts values of Kernel Ridge Regression
 
-function YPred = predictYKernel(XTrain,YTrain,XTest,K_fun,invK,K)
+function YPred = predictYKernel(XTrain,YTrain,XTest,K_fun,invK,K,hyp)
     nTrain = size(XTrain,2);
     nPred = size(XTest,2);
     YPred = zeros(1,nPred);
     ONEn = ones(nTrain,1);
     Cn = eye(nTrain) - 1/nTrain * (ONEn * ONEn');
-      
+    
+    switch hyp.kernel
+        case 'linear'
+            c = hyp.c;
+            K_fun = @(u,v) (c + u'*v);
+        case 'RBF'
+            sigma = hyp.k_sigma;
+            K_fun = @(u,v) exp(-((vecnorm(u-v)).^2)/(2*sigma.^2));
+    end   
+    
     Ktest = zeros(nTrain,1);
+
     for j = 1:nPred
         for i = 1:nTrain
             Ktest(i) = K_fun(XTrain(:,i),XTest(:,j));
