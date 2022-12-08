62be529b2c0e4067f11cfe63c8ebb4181031ff81
KelseyEng <74799106+KelseyEng@users.noreply.github.com>
2022-12-08 11:59:48 -0500

    Create more constants that hopefully speed up loop

--- predictYKernel.m
+++ predictYKernel.m
@@ -19,12 +19,14 @@ function YPred = predictYKernel(XTrain,YTrain,XTest,K_fun,invK,K,hyp)
     Ktest = zeros(nTrain,1);
     
     C1 = YTrain * Cn * invK * Cn';
+    C2 =  1/nTrain * YTrain * ONEn;
+    C3 = 1/nTrain * K * ONEn;
 
     for j = 1:nPred
         for i = 1:nTrain
             Ktest(i) = K_fun(XTrain(:,i),XTest(:,j));
         end
-        YPred(j) =  * (Ktest - 1/nTrain*K*ONEn) + 1/nTrain * YTrain * ONEn;
+        YPred(j) = C1 * (Ktest - C3) +C2;
         
     end
 
