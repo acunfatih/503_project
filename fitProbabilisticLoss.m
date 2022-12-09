28adb19ae5d3e0577bdf1d584b0c78ff9f95ecbb
Fatih Acun <fatih.acunn@gmail.com>
2022-12-09 10:36:27 -0500

    prob loss bug fix

--- fitProbabilisticLoss.m
+++ fitProbabilisticLoss.m
@@ -1,19 +1,19 @@
 function phi_y = fitProbabilisticLoss(YTrain ,print_graph)
     [f,xi] = ksdensity(YTrain);
+    [fnorm,fmin,frange] = norm_zero2one(f);
+    f_reversed = 1-fnorm;
     if print_graph
         histogram(YTrain);
         yyaxis left
         hold on;
         yyaxis right
         plot(xi,f,'LineWidth',2);
-        [fnorm,fmin,frange] = norm_zero2one(f);
-        f_reversed = 1-fnorm;
         plot(xi,f_reversed,'LineWidth',2);
         title('KDE of Normalized Prices');
         xlabel('Normalized Price');
         legend('data frequency', 'KDE', 'Ploss')
     end
-    phi_y = interp1(xi, f, YTrain);
+    phi_y = interp1(xi, f_reversed, YTrain);
 end
 
 
